71bd487b1bf2742e98444cb4268f8a36a6721f2b
Aleksander Kus <akus@stud.hs-heilbronn.de>
2018-05-05 22:08:43 +0200

    created PCData interface, interface for describing data

---
+++ src/main/java/de/exxcellent/challenge/data/PCData.java
@@ -0,0 +1,9 @@
+package de.exxcellent.challenge.data;
+
+/**
+ * interface for describing data that was readed by {@link de.exxcellent.challenge.io.PCFileReader}
+ *
+ * @author Aleksander Kus(akus@stud.hs-heilbronn.de)
+ */
+public interface PCData {
+}
