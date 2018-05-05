43b2d93b97006195522b94240c31cc1b25e3fbcb
Aleksander Kus <akus@stud.hs-heilbronn.de>
2018-05-05 23:45:39 +0200

    changed java-doc

--- src/main/java/de/exxcellent/challenge/io/PCFileReader.java
+++ src/main/java/de/exxcellent/challenge/io/PCFileReader.java
@@ -15,9 +15,10 @@ public interface PCFileReader {
 
     /**
      * @param fileName the file name of the wanted file to be parsed, must be readable and UTF-8
+     * @param type     the type of the incoming data must be one of {@link PCDataType} and not <code>null</code>
      * @return the data holder class that implements {@link PCData}
      */
-    List<PCData> parseFile(String fileName, PCDataType type) throws IOException;
+    List<PCData> parseFile(String fileName, PCDataType type);
 
 
 }
