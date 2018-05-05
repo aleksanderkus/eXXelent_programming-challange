99ee851a566553b0dd955d1196f76c9595b8a6e4
Aleksander Kus <akus@stud.hs-heilbronn.de>
2018-05-05 22:38:15 +0200

    implemented PCCsvData class, now its holding information about the day, maxTemt and minTemt with constructor, getter and setter

--- src/main/java/de/exxcellent/challenge/data/PCCsvData.java
+++ src/main/java/de/exxcellent/challenge/data/PCCsvData.java
@@ -6,4 +6,39 @@ package de.exxcellent.challenge.data;
  * @author Aleksander Kus(akus@stud.hs-heilbronn.de)
  */
 public class PCCsvData implements PCData {
+
+    private int dayOfMonth;
+    private int maxTemp;
+    private int minTemp;
+
+    public PCCsvData(int dayOfMonth, int maxTemp, int minTemp) {
+        this.dayOfMonth = dayOfMonth;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+    }
+
+
+    public int getDayOfMonth() {
+        return dayOfMonth;
+    }
+
+    public void setDayOfMonth(int dayOfMonth) {
+        this.dayOfMonth = dayOfMonth;
+    }
+
+    public int getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(int maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public int getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(int minTemp) {
+        this.minTemp = minTemp;
+    }
 }
--- src/main/java/de/exxcellent/challenge/io/PCCsvFileReader.java
+++ src/main/java/de/exxcellent/challenge/io/PCCsvFileReader.java
@@ -1,5 +1,6 @@
 package de.exxcellent.challenge.io;
 
+import com.sun.deploy.util.StringUtils;
 import de.exxcellent.challenge.data.PCData;
 
 import java.io.BufferedReader;
@@ -16,7 +17,15 @@ public class PCCsvFileReader extends PCAbstractFileReader {
 
     @Override
     public PCData parseFile(String fileName) {
-        
+        String fileContent = readFile(fileName);
+        // deleting first row
+        String[] lines = new String[fileContent.length()];
+        System.arraycopy(fileContent.split("\n"), 1, lines, 0, lines.length);
+
+
+        for (String line : lines){
+
+        }
         return null;
     }
 
