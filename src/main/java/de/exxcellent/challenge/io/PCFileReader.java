5776934b40a5f4be21d3dcb162a1f81383050bdf
Aleksander Kus <akus@stud.hs-heilbronn.de>
2018-05-05 22:10:53 +0200

    created methoddeclaration readFile(String fileName), for reading files

--- src/main/java/de/exxcellent/challenge/io/PCFileReader.java
+++ src/main/java/de/exxcellent/challenge/io/PCFileReader.java
@@ -1,9 +1,17 @@
 package de.exxcellent.challenge.io;
 
+import de.exxcellent.challenge.data.PCData;
+
+import java.io.IOException;
+
 /**
  * interface for reading files
  *
  * @author Aleksander Kus(akus@stud.hs-heilbronn.de)
  */
 public interface PCFileReader {
+
+
+    PCData readFile(String fileName) throws IOException;
+
 }
